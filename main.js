a54d7d5e5e36349d2df8112c4b442f711eeb8308
Mohamed Amine ZIDI <mohamedaminezidi1@gmail.com>
2020-09-10 13:48:23 +0100

    ajouter une animation

---
+++ main.js
@@ -0,0 +1,62 @@
+
+
+
+ if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please.Va-t-On ? " )) {
+
+
+window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_self');
+
+  }
+
+
+
+function myfunction() {
+
+document.body.style.backgroundColor= 'magenta'  ;
+document.getElementById("1").style.fontFamily= 'Comic Sans';
+document.getElementById("2").style.fontFamily= 'Comic Sans';
+document.getElementById("3").style.fontFamily= 'Comic Sans';
+document.getElementById("4").style.fontFamily= 'Comic Sans';
+document.getElementById("5").style.fontFamily= 'Comic Sans';
+document.getElementById("6").style.fontFamily= 'Comic Sans';
+document.getElementById("7").style.fontFamily= 'Comic Sans';
+document.getElementById("8").style.fontFamily= 'Comic Sans';
+document.getElementById("9").style.fontFamily= 'Comic Sans';
+
+document.getElementById("a").style.fontFamily='courier'; 
+document.getElementById("b").style.fontFamily='courrier'; 
+document.getElementById("c").style.fontFamily='courier'; 
+document.getElementById("d").style.fontFamily='courier'; 
+document.getElementById("e").style.fontFamily='courier'; 
+document.getElementById("f").style.fontFamily='courier'; 
+document.getElementById("g").style.fontFamily='courier'; 
+document.getElementById("h").style.fontFamily='courier'; 
+document.getElementById("i").style.fontFamily='courier'; 
+document.getElementById("j").style.fontFamily='courier'; 
+document.getElementById("k").style.fontFamily='courier'; 
+
+document.getElementById("1").style.color= ' green' ;
+document.getElementById("2").style.color= ' green' ;
+document.getElementById("3").style.color= ' green' ;
+document.getElementById("4").style.color= ' green' ;
+document.getElementById("5").style.color= ' green' ;
+document.getElementById("6").style.color= ' green' ;
+document.getElementById("7").style.color= ' green' ;
+document.getElementById("8").style.color= ' green' ;
+document.getElementById("9").style.color= ' green' ;
+
+
+document.getElementById("a").style.color= ' blue'; 
+document.getElementById("b").style.color= ' blue'; 
+document.getElementById("c").style.color= ' blue'; 
+document.getElementById("d").style.color= ' blue'; 
+document.getElementById("e").style.color= ' blue'; 
+document.getElementById("f").style.color= ' blue'; 
+document.getElementById("g").style.color= ' blue'; 
+document.getElementById("h").style.color= ' blue'; 
+document.getElementById("i").style.color= ' blue';
+document.getElementById("j").style.color= ' blue'; 
+document.getElementById("k").style.color= ' blue'; 
+
+
+}
